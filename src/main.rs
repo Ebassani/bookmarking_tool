808ec39f883316310ac395eec29da6518775f14f
Edu <eduardo.sbassani@gmail.com>
2023-01-16 10:16:51 +0200

    Adding search paths

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::response::Redirect;
+mod utils;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
+
+#[get("/command?<cmd>")]
+fn command(cmd: String) -> Redirect {
+    let command = utils::get_command(&cmd);
+    let redirect = match command {
+        "yt" => String::from("https://youtube.com"),
+        _ => String::from("https://google.com")
+        };
+    Redirect::to(redirect)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, command])
+}
+        
